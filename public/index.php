119f8d338ddbac2a587b6a9151a8e8342160a86f
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2023-12-26 10:43:49 -0300

    Criando uma pasta public para maior segurança

--- public/index.php
+++ public/index.php
@@ -1,19 +1,21 @@
 <?php
 
 if (!array_key_exists("PATH_INFO", $_SERVER) || $_SERVER["PATH_INFO"] === "/") {
-    require_once(__DIR__ . "/listagem-videos.php");
+    require_once(__DIR__ . "/../listagem-videos.php");
 } else if ($_SERVER["PATH_INFO"] === "/novo-video") {
     if ($_SERVER["REQUEST_METHOD"] === "GET") {
-        require_once(__DIR__ . "/formulario.php");
+        require_once(__DIR__ . "/../formulario.php");
     } else if ($_SERVER["REQUEST_METHOD"] === "POST") {
-        require_once(__DIR__ . "/novo-video.php");
+        require_once(__DIR__ . "/../novo-video.php");
     }
 } else if ($_SERVER["PATH_INFO"] === "/editar-video") {
     if ($_SERVER["REQUEST_METHOD"] === "GET") {
-        require_once(__DIR__ . "/formulario.php");
+        require_once(__DIR__ . "/../formulario.php");
     } else if ($_SERVER["REQUEST_METHOD"] === "POST") {
-        require_once(__DIR__ . "/editar-video.php");
+        require_once(__DIR__ . "/../editar-video.php");
     }
 } else if ($_SERVER["PATH_INFO"] === "/remover-video") {
-    require_once(__DIR__ . "/remover-video.php");
+    require_once(__DIR__ . "/../remover-video.php");
+} else {
+    http_response_code(404);
 }
