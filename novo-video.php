67f837d45fdf665be1d2be3e047d146a8c4ab044
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2023-12-26 10:09:13 -0300

    Redirecionando em caso de erro de valores

--- novo-video.php
+++ novo-video.php
@@ -4,13 +4,13 @@ $pdo = new PDO("sqlite:$dbPath");
 
 $url = filter_input(INPUT_POST, 'url', FILTER_VALIDATE_URL);
 if ($url === false) {
-  header('Location: /index.php?sucesso=0');
-  exit();
+    header('Location: /index.php?sucesso=0');
+    exit();
 }
 $titulo = filter_input(INPUT_POST, 'titulo');
 if ($titulo === false) {
-  header('Location: /index.php?sucesso=0');
-  exit();
+    header('Location: /index.php?sucesso=0');
+    exit();
 }
 
 $sql = 'INSERT INTO videos (url, title) VALUES (?, ?)';
